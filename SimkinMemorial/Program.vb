54e3dd7babb6131fe47bfd981d52484d0a6b3a53
andrewtgalloway <48169152+andrewtgalloway@users.noreply.github.com>
2019-10-05 19:24:28 -0700

    Simkin Memorial Code

---
+++ SimkinMemorial/Program.vb
@@ -0,0 +1,17 @@
+'A loop to run for eternity in memory of Professor Mark G. Simkin.
+Module SimkinMemorial
+	Sub Main()
+		Console.BackgroundColor = ConsoleColor.White
+		Console.ForegroundColor = ConsoleColor.DarkBlue
+		Console.Title = "Dr. Mark G. Simkin Code Memorial"
+
+		Dim SimkinIsRestingInPeace As Boolean = True
+
+		Do While SimkinIsRestingInPeace
+
+			Console.WriteLine("Rest in peace Dr. Simkin! Thank you for being awesome! You will forever live in our hearts!!!")
+
+		Loop
+
+	End Sub
+End Module
